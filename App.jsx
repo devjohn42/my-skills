2b01f4c513d124ee620b455cc7740b9e60c5be14
João Vitor <devjohncb@gmail.com>
2024-05-21 14:39:43 -0300

    chore: add typescript to project

--- App.jsx
+++ App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import Home from './app/pages/Home';
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar barStyle={'light-content'} backgroundColor='#af0' />
+      <Home />
+    </>
+  );
 }
--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { StatusBar, TextInput } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TextInput } from 'react-native';
 import { StyleSheet, Text, View, Platform, FlatList } from 'react-native';
 import Button from '../components/Button';
 import SkillCard from '../components/SkillCard';
@@ -7,15 +7,29 @@ import SkillCard from '../components/SkillCard';
 export default function Home() {
   const [newSkill, setNewSkill] = useState('');
   const [mySkills, setMySkills] = useState([]);
+  const [gretting, setGretting] = useState('');
 
   const handleAddNewSkill = () => {
     setMySkills([...mySkills, newSkill]);
   };
 
+  useEffect(() => {
+    const current = new Date().getHours();
+    if (current < 12) {
+      setGretting('Good Morning');
+    }
+    if (current >= 12 && current < 18) {
+      setGretting('Good Afternoon');
+    }
+    if (current > 18 && current <= 23) {
+      setGretting('Good Night');
+    }
+  }, []);
+
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={'light-content'} backgroundColor='#23232f' />
-      <Text style={styles.title}>My Skills</Text>
+      <Text style={styles.title}>Welcome, DevJohn</Text>
+      <Text style={styles.gretting}>{gretting}</Text>
 
       <TextInput
         style={styles.input}
@@ -58,6 +72,11 @@ const styles = StyleSheet.create({
     marginTop: 32,
     borderRadius: 8,
 
+    color: '#af0',
+    fontSize: 18,
+    fontWeight: '500'
+  },
+  gretting: {
     color: '#af0',
     fontSize: 18,
     fontWeight: '500'
